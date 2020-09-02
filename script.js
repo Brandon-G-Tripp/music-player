d941894f6a8327747c3c744ab68019ccb862def0
Brandon Tripp <brandongtripp@gmail.com>
2020-09-01 19:40:13 -0600

    initial styling of music play and basic play/pause functionality

---
+++ script.js
@@ -0,0 +1,30 @@
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+// Check if playing 
+
+let isPlaying = false;
+
+// Play
+function playSong() {
+   isPlaying = true;
+   playBtn.classList.replace('fa-play', 'fa-pause');
+   playBtn.setAttribute('title', 'Pause');
+   music.play();
+}
+// pause
+function pauseSong() {
+   isPlaying = false;
+   playBtn.setAttribute('title', 'Play');
+   playBtn.classList.replace('fa-pause', 'fa-play');
+   music.pause();
+}
+
+// Play or pause event listener
+
+playBtn.addEventListener('click', () => {
+   isPlaying ? pauseSong() : playSong()
+})
+
